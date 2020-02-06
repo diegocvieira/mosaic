7546b85a5f19c403d48c747031750a707e825140
diego.vieira <vieiracdiego@gmail.com>
2020-02-05 23:47:25 -0300

    Go back activate function

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -42,11 +42,8 @@ class StoreController extends Controller
 
     public function activate($store_id)
     {
-        // $store_id = is_array($store_id) ? implode(',', $store_id) : $store_id;
-        // $stores_id = _getStoreCookie() ? _getStoreCookie() . ',' . $store_id : $store_id;
-
-        $array = is_array($store_id) ? $store_id : [$store_id];
-        $stores_id = implode(',', array_merge($array, explode(',', Cookie::get('stores_id'))));
+        $store_id = is_array($store_id) ? implode(',', $store_id) : $store_id;
+        $stores_id = _getStoreCookie() ? _getStoreCookie() . ',' . $store_id : $store_id;
 
         Cookie::queue('stores_id', $stores_id, '525600');
 
